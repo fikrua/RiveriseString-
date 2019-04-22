bff79efafb3f86c8951d6121d7fc8462cf06923b
fikrua <fikruayal@gmail.com>
2019-04-22 15:58:43 -0400

    commit this

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,15 +6,45 @@ import java.util.Random;
 public class Main {
 
     public static void main(String[] args) {
+    	//to count the size of the string wirh outh using lingth method
+    	String newString = "fikruayaleneh melese";
+		char[] stringToCharArray = newString.toCharArray();
+		int s =0;
+		for(char elements:stringToCharArray){
+			s++;
+		}
 
-		Scanner scan = new Scanner(System.in);
-		System.out.println("enter your word ");
-		String testString = scan.nextLine();
-		char[] stringToCharArray = testString.toCharArray();
-		for(int i=testString.length()-1 ; i>=0 ; i--){
+		System.out.println("the size of the string is "+ s);
+    	Scanner scan = new Scanner(System.in);
+    	int [] arrayList = new int[2];
+    	System.out.println("enter a number");
+    	for(int i=0; i<arrayList.length; i++){
+    		arrayList[i] = scan.nextInt();
+			//System.out.println(arrayList[i]);
 
-			System.out.print( stringToCharArray[i]);
+		}
+
+		for(int element:arrayList){
+    		System.out.print(element + " ");
+
+		}
+		System.out.println();
+
+		int[] array1 = {1,7,6,5,9};
+    	int[] array2 = {2,7,6,3,4};
+    	for(int i =0 ;i<array1.length; i++){
+    		for(int j=0;j<array2.length;j++){
+    			if(array1[i]+array2[j]==13)
+    				System.out.println("("+array1[i]+ ","+array2[j]+")");
 			}
+		}
+		Student fikru = new Student();
+    	fikru.name("fikru");
+    	fikru.scor(98);
+    	fikru.desplay();
+
+
+
 
     }
 }
---
+++ src/com/company/Student.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Student {
+    String name;
+    int score;
+    public void name(String na){
+        name = na;
+        }
+        public void scor(int sc){
+        score = sc;
+        }
+        public void desplay(){
+        System.out.println(name+" " + score);
+        }
+}
